db3883a2c7d84e07af32476aa8508d74ba33d279
Triston Crouster <triston.c.0186@gmail.com>
2025-08-07 22:26:00 +0200

    fix(mypy): correct section header in mypy configuration and enhance type hinting in main.py
    - Change section header from [myPy] to [mypy] in mypy.ini for proper configuration recognition.
    - Update root function signature in main.py to include return type hint for better type checking.

--- src/main.py
+++ src/main.py
@@ -3,12 +3,14 @@ from fastapi import FastAPI
 app = FastAPI(
     title="Decision Maker API",
     description="API for decision making app",
-    version="0.1.0"
+    version="0.1.0",
 )
 
+
 @app.get("/")
-async def root():
+async def root() -> dict[str, str]:
     return {"message": "Decisin Maker API"}
+
 
 if __name__ == "__main__":
     import uvicorn
